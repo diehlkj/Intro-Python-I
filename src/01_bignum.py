f877048177a679a37f4b36f2a24a7de3cd0eabf0
Ken Diehl <diehlkj.99@gmail.com>
2020-09-02 21:17:45 -0400

    Completed 00, 01

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,6 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+
+#  ! The ** operator in Python is used to raise the number on the left to the power of the exponent of the right.
+print(2 ** 65536)
