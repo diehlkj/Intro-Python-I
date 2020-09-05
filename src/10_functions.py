15e99a80311b88edec7801c4f59f46f911c36191
Ken Diehl <diehlkj.99@gmail.com>
2020-09-04 23:43:23 -0400

    Completed 10

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,12 +1,47 @@
 # Write a function is_even that will return true if the passed-in number is even.
+def is_even(n):
+    if n%2 == 0:
+        return True
+    else:
+        return False
 
-# YOUR CODE HERE
+print(f"2 should be an even number: {is_even(2)}")
+print(f"1 should be an odd number: {is_even(1)}")
+print("\n")
+
 
 # Read a number from the keyboard
-num = input("Enter a number: ")
-num = int(num)
-
 # Print out "Even!" if the number is even. Otherwise print "Odd"
-
 # YOUR CODE HERE
 
+def enterNumber():
+    print("Enter a number, or 'q' to exit:")
+    num = input("> ")
+
+    try:
+        num = int(num)
+        if num % 2 == 0:
+            print("\n")
+            print(f"{num} is Even")
+            print("\n")
+        else:
+            print("\n")
+            print(f"{num} is Odd")
+            print("\n")
+    except ValueError:
+        try:
+            num = float(num)
+            print("\n")
+            print(f"{num} is not even or odd. It is a float")
+            print("\n")
+        except ValueError:
+            if num == "q" or num == "Q":
+                return
+            else:
+                print("\n")
+                print(f"'{num}' is not a number. It is a string.")
+                print("\n")
+    enterNumber()
+
+
+enterNumber()
